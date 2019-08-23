a43b7a73007cf83210d0047cbe7e90b9ea5604c8
William Eisenberger <weisenbe@kent.edu>
2019-08-23 14:35:06 -0400

    Update NCCJS2.js

--- js/NCCJS2.js
+++ js/NCCJS2.js
@@ -1,1 +1,18 @@
 XR.XrController.configure({imageTargets:['Anson','Cori','Daniel','Eleanor','Jessica','Joshua']})
+
+AFRAME.registerComponent('target-manager', {
+  init: function() {
+    const visible = {}
+	this.el.sceneEl.addEventListener('xrimagefound', e => {
+	  visible[e.detail.name] = true
+	  
+	  if (visible['Anson'] && visible['Cori']) {
+	  	console.log('Both targets are visible')
+	  }
+	})
+	
+	this.el.sceneEl.addEventListener('xrimagelost', e => {
+	  visible[e.detail.name] = false
+	})
+  }
+})
