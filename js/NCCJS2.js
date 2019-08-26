5bcaaef57aa6a7670bfcecf532c0fd83b92d15d6
billyxdude12 <weisenbe@kent.edu>
2019-08-26 13:02:58 -0400

    Update NCCJS2.js

--- js/NCCJS2.js
+++ js/NCCJS2.js
@@ -1,29 +1,13 @@
+XR.XrController.configure({disableWorldTracking:true});
+
 XR.XrController.configure({imageTargets:['Anson','Cori','Daniel','Eleanor','Jessica','Joshua']});
 
 var video = document.getElementById("currentVideo");
-
-AFRAME.registerComponent('target-manager', {
-  init: function() {
-    const visible = {}
-	this.el.sceneEl.addEventListener('xrimagefound', e => {
-	  visible[e.detail.name] = true
-	  
-	  if (visible['Anson'] && visible['Cori']) {
-	  	console.log('Both targets are visible')
-	  	video.play();
-	  }
-	})
-	
-	this.el.sceneEl.addEventListener('xrimagelost', e => {
-	  visible[e.detail.name] = false
-	})
-  }
-})
 
 
 AFRAME.registerComponent('button-changer', {
 	init:function(){
 		const visible = {true}
-		this.
+		this.el.sceneEl.addEventListener('xrimagefound')
 	}
 })
