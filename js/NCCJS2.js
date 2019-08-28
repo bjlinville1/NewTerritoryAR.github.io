823fb16c26361de01b951cd6f245c9de88027cfb
billyxdude12 <weisenbe@kent.edu>
2019-08-28 12:57:18 -0400

    Update NCCJS2.js

--- js/NCCJS2.js
+++ js/NCCJS2.js
@@ -8,17 +8,17 @@ var button2 = document.getElementById('button2');
 var button3 = document.getElementById('button3');
 
 
-function changeVideo1(){ 
-  document.getElementById("currentVideo").src="https://video.wixstatic.com/video/53058a_623bd1d60cac40ce961c8b5d3e1a5efe/720p/mp4/file.mp4"
-}
+// function changeVideo1(){ 
+//   document.getElementById("currentVideo").src="https://video.wixstatic.com/video/53058a_623bd1d60cac40ce961c8b5d3e1a5efe/720p/mp4/file.mp4"
+// }
 
-function changeVideo2(){
-document.getElementById("currentVideo").src="https://video.wixstatic.com/video/53058a_448d427e87d44e59899e077400c5f9fd/720p/mp4/file.mp4";
-}
+// function changeVideo2(){
+// document.getElementById("currentVideo").src="https://video.wixstatic.com/video/53058a_448d427e87d44e59899e077400c5f9fd/720p/mp4/file.mp4";
+// }
 
-function changeVideo3(){
-document.getElementById("currentVideo").src="https://video.wixstatic.com/video/53058a_f8e5399fa15349ad837124f2ca8c2b56/720p/mp4/file.mp4";
-}
+// function changeVideo3(){
+// document.getElementById("currentVideo").src="https://video.wixstatic.com/video/53058a_f8e5399fa15349ad837124f2ca8c2b56/720p/mp4/file.mp4";
+// }
 
 
 AFRAME.registerComponent('button-changer', {
@@ -28,7 +28,9 @@ AFRAME.registerComponent('button-changer', {
 			visible[xrimagefound.detail.name] = true
 
 			if (visible['Anson']){
-				changeVideo1.currentVideo.src="https://video.wixstatic.com/video/53058a_513ae7c02ec5409a9eb41d2e0e97d6fb/720p/mp4/file.mp4"
+				document.getElementById('button1').onclick= function(){
+				document.getElementById('currentVideo').src="https://video.wixstatic.com/video/53058a_513ae7c02ec5409a9eb41d2e0e97d6fb/720p/mp4/file.mp4";
+				}	
 			}
 		})
 
